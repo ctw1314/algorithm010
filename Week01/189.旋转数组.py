3dd2b48e39380c25212b58af729d729ab967b08d
gaoyutang <13834272287@163.com>
2020-06-15 10:35:58 +0800

    189add

--- Week01/189.旋转数组.py
+++ Week01/189.旋转数组.py
@@ -3,6 +3,7 @@
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        k = k % len(nums)
         for i in range(k):
             nums.insert(0,nums.pop())
 
@@ -11,6 +12,7 @@ class Solution:
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        k = k % len(nums)
         nums[:] = nums[len(nums) - k:] + nums[:len(nums) - k]
 
 
